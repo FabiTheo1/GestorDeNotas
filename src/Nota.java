67c95dc4d70973206e03288adda39e5529b03a60
Fabian <fabianmaster531@gmail.com>
2026-03-10 21:49:55 +0000

    Primer commit: empezando con la creación de Nota.java (sin terminar)

---
+++ src/Nota.java
@@ -0,0 +1,13 @@
+import java.io.Serializable;
+import java.util.UUID;
+
+public class Nota implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String id;
+    private String titulo;
+    private String contenido;
+
+    
+
+}
