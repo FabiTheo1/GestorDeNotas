bb35ddc2f9f1860f66bb2b14cd6f6dde954b79d6
Fabian <fabianmaster531@gmail.com>
2026-03-10 22:01:07 +0000

    Añado clase Nota terminada y clase Usuario empezada

--- src/Nota.java
+++ src/Nota.java
@@ -8,6 +8,36 @@ public class Nota implements Serializable {
     private String titulo;
     private String contenido;
 
-    
+    public Nota(String titulo, String contenido) {
+        this.id = UUID.randomUUID().toString(); //Esta parte genera un id único para cada nota.
+        this.titulo = titulo;
+        this.contenido = contenido;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getContenido() {
+        return contenido;
+    }
+
+    public void setContenido(String contenido) {
+        this.contenido = contenido;
+    }
+
+    //Este método muestra la lista Jlist de la interfaz.
+    @Override
+    public String toString(){
+        return titulo;
+    }
 
 }
---
+++ src/Usuario.java
@@ -0,0 +1,8 @@
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Usuario implements Serializable {
+private static final long serialVersionUID = 1L;
+
+}
